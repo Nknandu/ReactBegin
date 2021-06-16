9ab732308bac3f879c077596272884e50d0d5e20
Nandakumar <nandakumar.nk@iroidtechnologies.com>
2021-06-16 21:02:34 +0530

    components are setuped

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Topbar from "./components/tobar/topbar";
+import Work from "./components/works/works";
 
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Topbar />
+      
+      <div className="section">
+      <Intro/>
+      <Portfolio/>
+      <Work/>
+      <Testimonials/>
+      <Contact/>
+      </div>
     </div>
   );
 }
